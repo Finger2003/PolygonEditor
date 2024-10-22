155c05a8e7de030ed11bfe3f32c30e90b1fefc80
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-10-22 14:12:51 +0200

    Change Bezier curve drawing algorithm

--- Lab1/LineDrawers/AbstractLineDrawer.cs
+++ Lab1/LineDrawers/AbstractLineDrawer.cs
@@ -10,24 +10,49 @@
         public abstract void DrawHorizontalLine(Point start, Point end);
         public abstract void DrawVerticalLine(Point start, Point end);
         public abstract void DrawStraightLine(Point start, Point end);
-        public void DrawBezierCurve(Point V0, Point V3, Point V1, Point V2)
+        public void DrawBezierCurve(Point v0, Point v3, Point v1, Point v2)
         {
-            int A0x = V0.X;
-            int A0y = V0.Y;
-            int A1x = 3 * (V1.X - V0.X);
-            int A1y = 3 * (V1.Y - V0.Y);
-            int A2x = 3 * (V2.X - 2 * V1.X + V0.X);
-            int A2y = 3 * (V2.Y - 2 * V1.Y + V0.Y);
-            int A3x = V3.X - 3 * V2.X + 3 * V1.X - V0.X;
-            int A3y = V3.Y - 3 * V2.Y + 3 * V1.Y - V0.Y;
+            //int a0x = v0.X;
+            //int a0y = v0.Y;
+            int a1x = 3 * (v1.X - v0.X);
+            int a1y = 3 * (v1.Y - v0.Y);
+            int a2x = 3 * (v2.X - 2 * v1.X + v0.X);
+            int a2y = 3 * (v2.Y - 2 * v1.Y + v0.Y);
+            int a3x = v3.X - 3 * v2.X + 3 * v1.X - v0.X;
+            int a3y = v3.Y - 3 * v2.Y + 3 * v1.Y - v0.Y;
 
 
-            double oldX = V0.X;
-            double oldY = V0.Y;
-            for (double t = 0; t <= 1; t += 0.01)
+            int deltaX = v3.X - v0.X;
+            int deltaY = v3.Y - v0.Y;
+
+            int loopIterations = (int) Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            double dt = 1 / (double) loopIterations;
+            double dt2 = dt * dt;
+            double dt3 = dt2 * dt;
+
+            double dddX = 6 * a3x * dt3;
+            double dddY = 6 * a3y * dt3;
+            double ddX = dddX + 2 * a2x * dt2;
+            double ddY = dddY + 2 * a2y * dt2;
+            double dX = a3x * dt3 + a2x * dt2 + a1x * dt;
+            double dY = a3y * dt3 + a2y * dt2 + a1y * dt;
+
+
+            double oldX, x, oldY, y;
+            oldX = x = v0.X;
+            oldY = y = v0.Y;
+
+            for (int i = 0; i < loopIterations; i++)
             {
-                double x = ((A3x * t + A2x) * t + A1x) * t + A0x;
-                double y = ((A3y * t + A2y) * t + A1y) * t + A0y;
+                x += dX;
+                y += dY;
+                dX += ddX;
+                dY += ddY;
+                ddX += dddX;
+                ddY += dddY;
+                //double x = ((a3x * t + a2x) * t + a1x) * t + a0x;
+                //double y = ((a3y * t + a2y) * t + a1y) * t + a0y;
                 G.DrawLine(Pens.Black, (float)oldX, (float)oldY, (float)x, (float)y);
                 oldX = x;
                 oldY = y;
@@ -35,11 +60,11 @@
 
 
 
-            G.DrawEllipse(Pens.Black, V1.X - 2, V1.Y - 2, 4, 4);
-            G.DrawEllipse(Pens.Black, V2.X - 2, V2.Y - 2, 4, 4);
-            G.DrawLine(DottedPen, V0, V1);
-            G.DrawLine(DottedPen, V1, V2);
-            G.DrawLine(DottedPen, V2, V3);
+            G.DrawEllipse(Pens.Black, v1.X - 2, v1.Y - 2, 4, 4);
+            G.DrawEllipse(Pens.Black, v2.X - 2, v2.Y - 2, 4, 4);
+            G.DrawLine(DottedPen, v0, v1);
+            G.DrawLine(DottedPen, v1, v2);
+            G.DrawLine(DottedPen, v2, v3);
         }
     }
 }
